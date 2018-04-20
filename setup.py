e09bfe47bb4d7fe4446612a5c6d689a4586d4db5
Brian Lewis <brian@bayesimpact.org>
2018-04-20 12:14:38 -0700

    Release v0.0.1

---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+import setuptools
+
+
+def _get_long_description():
+    with open('README.md', 'r') as f:
+        desc = f.read()
+    return desc
+
+
+def _get_install_requires():
+    # TODO: Implement this function to read from requirements.txt.
+    pass
+
+setuptools.setup(
+    name='aceso',
+    version='0.0.1',
+    description='''
+        Lightweight package to calculate 2SFCA and other measures of spatial accessibility
+        '''.strip(),
+    long_description=_get_long_description(),
+    long_description_content_type='text/markdown',
+    url='http://github.com/tetraptych/aceso',
+    keywords=['geospatial', 'geo', 'gis', '2SFCA', 'gravity', 'access'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering :: GIS',
+    ],
+    author='Brian Lewis',
+    author_email='brianburkelewis@gmail.com',
+    license='MIT',
+    packages=['aceso'],
+    install_requires=[
+        'numpy>=1.11.0',
+    ],
+    python_requires='>=2.7'
+)
